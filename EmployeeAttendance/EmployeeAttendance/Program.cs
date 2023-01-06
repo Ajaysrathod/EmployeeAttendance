ad5c88f3f94647c5ca403abcddb318ce11e617c1
ajayrr29@gmail.com <ajayrr29@gmail.com>
2023-01-06 21:50:52 +0530

    EmployeeAttendance program complete

---
+++ EmployeeAttendance/EmployeeAttendance/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace EmployeeAttendance
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Emoplyee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Emoplyee is Absent");
+            }
+            Console.WriteLine("Hello, World!");
+
+        }
+    }
+}
